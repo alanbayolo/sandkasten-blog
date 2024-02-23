4523e9542a4918fd09ae8038eb77293b9c12519a
Alan <60156381+alanbayolo@users.noreply.github.com>
2024-02-23 14:10:34 -0500

    test without base parameter at astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sandkasten.github.io',
-  base: '/sandkasten-blog',
   integrations: [tailwind()]
 });
